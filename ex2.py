8817fd4756178150b522d1b9b3f9087e4a7c226c
BabyLotuss <1800rvregxd@gmail.com>
2021-09-16 15:34:35 +1100

    hw

---
+++ ex2.py
@@ -0,0 +1,14 @@
+list_1 = ['в', '5', 'часов', '17', 'минут', 'температура', 'воздуха', 'была', '+5', 'градусов']
+new_list = []
+
+for i in list_1:
+    if i.replace("+", "").replace("-", "").isdigit():
+        if i.isdigit():
+            new_list.append(f"'{int(i):02}'")
+        else:
+            new_list.append(f"'{i[0]}{int(i[1:]):02}'")
+    else:
+        new_list.append(i)
+
+print(new_list)
+print(" ".join(new_list))
